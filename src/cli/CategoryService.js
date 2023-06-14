a780481e5e195e4d86b031225847acee4fbc691f
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2023-06-14 14:09:28 -0300

    Detalhar uma Categoria

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -6,6 +6,20 @@ export class CategoryService {
 
         console.log(`Response status: ${response.status}`);
 
+        if(response.status === 404) throw new Error('Nenhum categoria foi encontrada');
+
+        return response.json();
+    }
+
+    static async findCategoryById(id) {
+
+        const response = await fetch(`http://localhost:3000/categories/${id}`);
+
+
+        console.log(`Response status: ${response.status}`);
+
+        if(response.status === 404) throw new Error('Categoria não encontrada');
+
         return response.json();
     }
 
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,28 +1,38 @@
 import { CategoryService } from "./CategoryService.js";
 
-const path = process.argv;
+const commandLineArguments = process.argv;
 
 
-async function performRequestedCommand(command) {
+async function performRequestedCommand(command, additionalParameter = null) {
     switch (command) {
         case '--listarCategorias':
+            // node ./src/cli/cli.js --listarCategorias
             return await CategoryService.findCategories();
+        case '--recuperarCategoriaPorId':
+            // node ./src/cli/cli.js --recuperarCategoriaPorId 1
+            const id = additionalParameter;
+            return await CategoryService.findCategoryById(id);
         default:
-            return 'Comando inválido';
+            throw new Error('Comando inválido');
     }
 }
 
-function printResult(resultToBePrinted, requestedCommand){
+function printResult(resultToBePrinted, requestedCommand) {
     console.log(`\n***** Resultado do comando: '${requestedCommand}' *****\n`, resultToBePrinted);
 }
 
 async function processarComando(commandLineArguments) {
     const requestedCommand = commandLineArguments[2];
+    const additionalParameter = commandLineArguments[3];
 
-    const result = await performRequestedCommand(requestedCommand);
+    try {
+        const result = await performRequestedCommand(requestedCommand, additionalParameter);
 
-    printResult(result, requestedCommand);
+        printResult(result, requestedCommand);
+    } catch (error) {
+        console.log(`Erro: ${error.message}`);
+    }
 }
 
 
-processarComando(path);
+processarComando(commandLineArguments);
