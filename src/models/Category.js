c403b4a5cc931ddf0d2723487dd95c514c9fbbcd
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2023-06-22 15:27:02 -0300

    Remoção de comentários

--- src/models/Category.js
+++ src/models/Category.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const categorySchema = new mongoose.Schema(
   {
-    // _id: { type: mongoose.Schema.Types.ObjectId },
     nome: { type: String, required: true, validate: /^(?![0-9])[a-zA-Z0-9]{3,}$/ },
     status: { type: String },
   },
