4325e17708eda6accdad85e41c6d5df2b980c5b4
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2023-06-22 15:26:54 -0300

    Remoção de comentários

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,30 +1,23 @@
-import categories from '../models/Category.js';
+import Category from '../models/Category.js';
 
 class CategoryController {
   static async findCategories(req, res) {
     try {
-      const categoriesDocuments = await categories.find();
+      const categoriesDocuments = await Category.find();
+
       if (categoriesDocuments.length === 0) throw new Error('Nenhuma categoria encontrada');
 
       return res.status(200).json(categoriesDocuments);
     } catch (err) {
       return res.status(404).send({ message: err.message });
     }
-
-    // categories.find((err, categoriesFound) => {
-    //   if (categoriesFound.length === 0) {
-    //     return res.status(404).send({ message: 'Nenhuma categoria encontrada' });
-    //   }
-
-    //   return res.status(200).json(categoriesFound);
-    // });
   }
 
   static async findCategoryById(req, res) {
     try {
       const id = req.params.id;
 
-      const category = await categories.findById(id).exec();
+      const category = await Category.findById(id).exec();
 
       return res.status(200).json(category);
     } catch (err) {
@@ -35,18 +28,12 @@ class CategoryController {
   static async createCategory(req, res) {
     try {
       const categoryToBeCreated = req.body;
-      const createdCategory = await categories.create(categoryToBeCreated);
+      const createdCategory = await Category.create(categoryToBeCreated);
 
       return res.status(201).json(createdCategory);
     } catch (err) {
       return res.status(400).send({ message: `${err.message} - Falha ao cadastrar categoria` });
     }
-
-    // const category = new categories(req.body);
-
-    // category.save(() => {
-    //   res.status(201).send(category.toJSON());
-    // });
   }
 }
 
