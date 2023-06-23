6baf04260b967c7be5769f4096193d9f28391f84
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2023-06-23 14:05:25 -0300

    Executar a API pelo Docker

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://admin:secret@localhost:27017/ecomm?authSource=admin');
+mongoose.connect(`mongodb://admin:secret@${process.env.MONGO_HOST || 'localhost'}:27017/ecomm?authSource=admin`);
 
 const db = mongoose.connection;
 
